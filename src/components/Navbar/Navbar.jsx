c3f0584210c46bb1d48d8e66313c38676a38230c
nilu332 <niloferp332@gmail.com>
2025-09-26 09:34:51 +0530

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -36,10 +36,10 @@ const toggleMenu = () =>{
             {/* Desktop Menu */}
             <ul className='md:flex items-center gap-x-15 hidden'>
                 <li>
-                   <a href='#' className='font-semibold tracking-wider text-orange-600'>Home</a>
+                   <a href='#' className='font-semibold tracking-wider text-orange-500'>Home</a>
                 </li>
                 <li>
-                   <a href='#' className='font-semibold tracking-wider text-zinc-700 hover:text-orange-500'>About us</a>
+                   <a href='#' className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>About us</a>
                 </li>
                 <li>
                    <a href='#' className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>Process</a>
